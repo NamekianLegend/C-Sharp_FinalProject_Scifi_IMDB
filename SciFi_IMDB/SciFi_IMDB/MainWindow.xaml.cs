80551c382587c09ed37124ff67410e3687f46f40
Josh Leslie <joshleslie1993@gmail.com>
2026-04-08 11:05:01 -0300

    Added EF packages required for database work.

--- SciFi_IMDB/SciFi_IMDB/MainWindow.xaml.cs
+++ SciFi_IMDB/SciFi_IMDB/MainWindow.xaml.cs
@@ -1,18 +1,12 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using SciFi_IMDB.ViewModels;
 
 namespace SciFi_IMDB
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// Scaffolding command:
+    /// Scaffold-DbContext -Connection "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=IMDB;Integrated Security=True;Encrypt=True;Trust Server Certificate=True", -Provider Microsoft.EntityFrameworkCore.SqlServer ,-Tables Titles, Genres, Title_Genres, Names, Principals, Ratings ,-OutputDir Models\Generated ,-ContextDir Data\Generated ,-Namespace SciFi_IMDB ,-ContextNamespace SciFi_IMDB ,-Force
     /// </summary>
     public partial class MainWindow : Window
     {
